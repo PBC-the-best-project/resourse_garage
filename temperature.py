7a38fecc92a208f0184416514174f8c2e32d5230
howard0191 <75771098+howard0191@users.noreply.github.com>
2020-12-21 12:01:55 +0800

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 a = int(input())  # 地點
-b = int(input())  # 星座
+b = int(input())  # 1今日/2明日/3本週/4本月/5本年
+c = int(input())  # 星座：1牡羊/2金牛/3雙子/4巨蟹/5獅子/6處女/7天秤/8天蠍/9射手/10摩羯/11水瓶/12雙魚
+
 url1 = 'https://www.cwb.gov.tw/V8/C/W/Town/Town.html?TID=6300400'
 r1 = requests.get(url1)
 if a <= 9:
@@ -13,7 +15,7 @@ else:
         str(a) + ':1:TW?Goto=Redirected'
 
 r2 = requests.get(url2)
-url3 = 'http://tw.xingbar.com/cgi-bin/v5starfate2?fate=1&type=' + str(b)
+url3 = 'http://tw.xingbar.com/cgi-bin/v5starfate2?fate=' + str(b) + '&type=' + str(c)
 r3 = requests.get(url3)
 
 soup1 = BeautifulSoup(r1.text, 'html.parser')
@@ -30,17 +32,22 @@ num3 = soup2.find_all('div', attrs=attr2)  # 天氣
 attr3 = {'class': 'CurrentConditions--location--1Ayv3'}
 num4 = soup2.find_all('h1', attrs=attr3)  # 地點
 attr4 = {'class': 'dotRbox dottxt'}
-num5 = soup3.find_all('div', attrs=attr4)  # 幸運物
+num5 = soup3.find_all('div', attrs=attr4)  # 愛情＋工作＋幸運色＋幸運物
+attr5 = {'class': 'dotLbox dottxt'}
+num6 = soup3.find_all('div', attrs=attr5)  # 心情＋財運＋健康＋開運方位
 
 for value in num2:
     list1.append(value.get_text())  # 溫度＋溫度值
 
 string1 = str(num4[0].get_text()) + '：' + str(num3[0].get_text())
 string2 = str()
+string3 = str()
 
 for a in range(len(list1)):
     string2 += list1[a]
 
+for a in range(len(num5)):
+    string3 += num6[a].get_text() + '  ' + num5[a].get_text() + '\n'
 # output
 now = datetime.datetime.now()  # 2019-04-11 14:18:41.629019
 otherStyleTime = now.strftime("%Y-%m-%d %H:%M:%S")  # 2019-04-11 14:18:41
@@ -48,7 +55,7 @@ print(otherStyleTime)
 
 print(string1)  # 地點 & 天氣
 print(string2)
-print(num5[3].get_text())
+print(string3)
 
 temperature = int(list1[1][:-1])  # 比較用
 print(temperature)  # 當下溫度數值
